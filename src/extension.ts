5df74589e978099c24dcd2ec24fd6dcb7fb3c5e6
Matt Stewart <smtp.matt@gmail.com>
2020-10-23 21:44:13 +1100

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { exec } from "promisify-child-process";
-import { execFile } from "child_process";
+import * as child_proc from "promisify-child-process";
 import { Range, TextDocument } from "vscode";
-
-interface RejigSettings {
-  prefixGroups: String[];
-  displayImportGroupTitles: Boolean;
-  displayImportBorderTop: Boolean;
-  displayImportBorderBottom: Boolean;
-}
+import { posix } from "path";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "rejig-vscode" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     "rejig-vscode.runFormatter",
-    () => {
+    async () => {
       // The code you place here will be executed every time your command is executed
 
       if (!vscode.window.activeTextEditor) {
@@ -33,17 +25,16 @@ export function activate(context: vscode.ExtensionContext) {
 
       const editor = vscode.window.activeTextEditor;
       const fullRange = fullDocumentRange(editor.document);
+      const config = vscode.workspace.getConfiguration("rejig");
 
       try {
         const args = [];
-        const config = vscode.workspace.getConfiguration("rejig");
 
-        // cli args requires each element to be in string sep by whitespace
         if (config.prefixGroups.length > 0) {
+          // cli arg requires string
           args.push("--prefixes", config.prefixGroups?.join(" "));
         }
 
-        // remaining config are all flags, if provided setting is enabled
         if (config.displayImportGroupTitles) {
           args.push("--titles");
         }
@@ -53,62 +44,43 @@ export function activate(context: vscode.ExtensionContext) {
         if (config.displayImportBorderBottom) {
           args.push("--border-bottom");
         }
-        args.push("--stdin");
+        args.push("--stdin", editor.document.fileName);
         args.push(`--${editor.document.languageId}`);
 
-        console.log(args);
-        const child = execFile("/home/matt/dev/rejig/build/rejig", args, {
-          encoding: "utf-8",
-        });
+        const child = child_proc.execFile(
+          "/home/matt/dev/rejig/build/rejig",
+          args
+        );
 
         child.stdin?.write(editor.document.getText());
         child.stdin?.end();
 
-        child.stdout?.on("data", (data) => {
+        const { stdout, stderr } = await child;
+        const err = String(stderr);
+
+        if (err.length > 0) {
+          onError(config, editor, err);
+          return;
+        }
+
+        const response = JSON.parse(String(stdout));
+
+        if (response.status === "ok") {
           editor.edit((editBuilder) => {
-            editBuilder.replace(fullRange, data);
+            editBuilder.replace(fullRange, response.data);
           });
-        });
-        // console.log("write to stdin");
-        // const { stdout, stderr } = await child;
+          return;
+        }
 
-        // console.log("stdout dump");
-        // console.log(String(stdout?.read()));
-
-        // console.log("stderr dump");
-        // console.log(String(stderr));
-      } catch (e) {}
-
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from rejig-vscode-extension!"
-      );
+        onError(config, editor, response.data);
+      } catch (e) {
+        onError(config, editor, e.toString());
+      }
     }
   );
 
   context.subscriptions.push(disposable);
 }
-
-// // this method is called when your extension is activated
-// // your extension is activated the very first time the command is executed
-// export function activate(context: vscode.ExtensionContext) {
-
-// // Use the console to output diagnostic information (console.log) and errors (console.error)
-// // This line of code will only be executed once when your extension is activated
-// console.log('Congratulations, your extension "rejig-vscode" is now active!');
-
-// // The command has been defined in the package.json file
-// // Now provide the implementation of the command with registerCommand
-// // The commandId parameter must match the command field in package.json
-// let disposable = vscode.commands.registerCommand('rejig-vscode.helloWorld', () => {
-// // The code you place here will be executed every time your command is executed
-
-// // Display a message box to the user
-// vscode.window.showInformationMessage('Hello World from rejig-vscode-extension!');
-// });
-
-// context.subscriptions.push(disposable);
-// }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
@@ -116,4 +88,21 @@ export function deactivate() {}
 function fullDocumentRange(document: TextDocument): Range {
   const last: number = document.lineCount - 1;
   return new Range(0, 0, last, document.lineAt(last).text.length);
+}
+
+async function onError(
+  config: vscode.WorkspaceConfiguration,
+  editor: vscode.TextEditor,
+  body: string
+) {
+  if (config.writeErrorFile) {
+    const wsUri = vscode.workspace.getWorkspaceFolder(editor.document.uri)?.uri;
+    if (wsUri) {
+      const fileUri = wsUri.with({
+        path: posix.join(wsUri.path, "rejig-error.txt"),
+      });
+      await vscode.workspace.fs.writeFile(fileUri, Buffer.from(body, "utf8"));
+      vscode.window.showErrorMessage("Rejig: See 'rejig-error.txt' for details");
+    }
+  }
 }
